324f5dc5844ee4348d2f65cdeb50bc5a5e564fbe
Stanislav Yurchenko <syurchenko@summit-tech.ca>
2025-09-09 16:41:44 -0400

    Change timeout logic

--- src/internal/sio_client_impl.h
+++ src/internal/sio_client_impl.h
@@ -27,11 +27,16 @@ typedef websocketpp::config::summit_tls_client client_config;
 namespace sio
 {
     using namespace websocketpp;
-    
+
     typedef websocketpp::client<client_config> client_type;
-    
+#ifdef _SAL_TIME_H
+    typedef std::unique_ptr<SAL::timer> TIMER;
+#else
+    typedef std::unique_ptr<asio::steady_timer> TIMER;
+#endif
+
     class client_impl {
-        
+
     protected:
         enum con_state
         {
@@ -40,33 +45,33 @@ namespace sio
             con_closing,
             con_closed
         };
-        
+
         client_impl(ProtocolVersion version);
-        
+
         ~client_impl();
-        
+
         //set listeners and event bindings.
 #define SYNTHESIS_SETTER(__TYPE__,__FIELD__) \
     void set_##__FIELD__(__TYPE__ const& l) \
         { m_##__FIELD__ = l;}
-        
+
         SYNTHESIS_SETTER(client::con_listener,open_listener)
-        
+
         SYNTHESIS_SETTER(client::con_listener,fail_listener)
 
         SYNTHESIS_SETTER(client::reconnect_listener,reconnect_listener)
 
         SYNTHESIS_SETTER(client::con_listener,reconnecting_listener)
-        
+
         SYNTHESIS_SETTER(client::close_listener,close_listener)
-        
+
         SYNTHESIS_SETTER(client::socket_listener,socket_open_listener)
-        
+
         SYNTHESIS_SETTER(client::socket_listener,socket_close_listener)
-        
+
 #undef SYNTHESIS_SETTER
-        
-        
+
+
         void clear_con_listeners()
         {
             m_open_listener = nullptr;
@@ -75,26 +80,26 @@ namespace sio
             m_reconnect_listener = nullptr;
             m_reconnecting_listener = nullptr;
         }
-        
+
         void clear_socket_listeners()
         {
             m_socket_open_listener = nullptr;
             m_socket_close_listener = nullptr;
         }
-        
+
         // Client Functions - such as send, etc.
         void connect(const std::string& uri, const std::map<std::string, std::string>& queryString,
                      const std::map<std::string, std::string>& httpExtraHeaders, const message::ptr& auth);
-        
+
         sio::socket::ptr const& socket(const std::string& nsp);
-        
+
         // Closes the connection
         void close();
-        
+
         void sync_close();
-        
+
         bool opened() const { return m_con_state == con_opened; }
-        
+
         std::string const& get_sessionid() const { return m_sid; }
 
         void set_reconnect_attempts(unsigned attempts) {m_reconn_attempts = attempts;}
@@ -108,48 +113,44 @@ namespace sio
         void set_logs_quiet();
 
         void set_logs_verbose();
-		
+
         // void set_proxy_basic_auth(const std::string& uri, const std::string& username, const std::string& password);
 
     protected:
         void send(packet& p);
-        
+
         void remove_socket(std::string const& nsp);
-        
-        // asio::io_service& get_io_service();
-        
+#ifndef _SAL_TIME_H
+        asio::io_service& get_io_service();
+#endif
         void on_socket_closed(std::string const& nsp);
-        
+
         void on_socket_opened(std::string const& nsp);
-        
+
     private:
         void run_loop();
 
         void connect_impl(const std::string& uri, const std::string& query);
 
         void close_impl(close::status::value const& code,std::string const& reason);
-        
+
         void send_impl(std::shared_ptr<const std::string> const&  payload_ptr,frame::opcode::value opcode);
-        
-        // void ping(const asio::error_code& ec);
-        
-        void timeout_ping(const std::error_code& ec);
 
-        void timeout_send_ping(const std::error_code& ec);
+        void timeout_send(const std::error_code& ec);
 
-        void timeout_wait_pong(const std::error_code& ec);
+        void timeout_wait(const std::error_code& ec);
 
         void timeout_reconnect(std::error_code const& ec);
 
         unsigned next_delay() const;
 
         socket::ptr get_socket_locked(std::string const& nsp);
-        
+
         void sockets_invoke_void(void (sio::socket::*fn)(void));
-        
+
         void on_decode(packet const& pack);
         void on_encode(bool isBinary,shared_ptr<const string> const& payload);
-        
+
         //websocket callbacks
         void on_fail(connection_hdl con);
 
@@ -170,18 +171,16 @@ namespace sio
 
         void clear_timers();
 
-        void update_ping_timeout_timer();
+        void update_timeout_timer();
 
-        void update_send_ping_timer();
-        
-        void update_wait_pong_timeout_timer();
+        void update_send_timer();
 
-        #if SIO_TLS
+#if SIO_TLS
         typedef websocketpp::lib::shared_ptr<asio::ssl::context> context_ptr;
-        
+
         context_ptr on_tls_init(connection_hdl con);
-        #endif
-        
+#endif
+
         // Percent encode query string
         std::string encode_query_string(const std::string &query);
 
@@ -200,32 +199,30 @@ namespace sio
 
         unsigned int m_ping_interval;
         unsigned int m_ping_timeout;
-        
+
         std::unique_ptr<std::thread> m_network_thread;
-        
+
         packet_manager m_packet_mgr;
-        
-        std::unique_ptr<SAL::timer> m_ping_timeout_timer;
+        typedef void (sio::client_impl::*func_ptr)(const std::error_code&) ;
+        void update_timer(TIMER &timer, int timeout, func_ptr name);
+        void reset_timer(TIMER &timer);
+        TIMER m_send_timer;
+        TIMER m_timeout_timer;
+        TIMER m_reconn_timer;
 
-        std::unique_ptr<SAL::timer> m_send_ping_timer;
-
-        std::unique_ptr<SAL::timer> m_wait_pong_timeout_timer;
-
-        std::unique_ptr<SAL::timer> m_reconn_timer;
-        
         con_state m_con_state;
-        
+
         client::con_listener m_open_listener;
         client::con_listener m_fail_listener;
         client::con_listener m_reconnecting_listener;
         client::reconnect_listener m_reconnect_listener;
         client::close_listener m_close_listener;
-        
+
         client::socket_listener m_socket_open_listener;
         client::socket_listener m_socket_close_listener;
-        
+
         std::map<const std::string,socket::ptr> m_sockets;
-        
+
         std::mutex m_socket_mutex;
 
         unsigned m_reconn_delay;
@@ -245,4 +242,3 @@ namespace sio
     };
 }
 #endif // SIO_CLIENT_IMPL_H
-
